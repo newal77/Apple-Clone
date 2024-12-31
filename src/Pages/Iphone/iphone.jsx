ad69cb7dab1ebac434374e7b2f0417e6947b0d6d
newal77 <waltanewal@gmail.com>
2024-12-31 12:59:37 -0700

    updates

--- src/Pages/Iphone/iphone.jsx
+++ src/Pages/Iphone/iphone.jsx
@@ -1,55 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import data from "./products.json";
 
 function Iphone() {
   const [products, setProducts] = useState([]);
 
+  //   useEffect(() => {
+  //     // fetch("http://localhost:1111/products")
+  //     //   .then((res) => res.json())
+  //     //   .then((data) => setProducts(data));
+  //         setProducts(data);
+  //   }, []);
+
   useEffect(() => {
-    fetch("http://localhost:1122/iphone")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    setProducts(data);
   }, []);
 
-  console.log(products);
-  let order = 1;
   return (
-    <>
+    <div>
       <section className="internal-page-wrapper top-100">
         <div className="container">
           <div className="row justify-content-center text-center">
             <div className="col-12">
-              <div className="title-wraper bold">Iphones</div>
+              <div className="title-wrapper bold">iPhones</div>
               <div className="brief-description">
                 The best for the brightest.
               </div>
             </div>
           </div>
-          {products.map((product) => {
-            let id = product.id;
-            let title = product.title;
-            let img = product.img;
-            let Brief = product.brief;
-            let StartPrice = product.startingPrice;
-            let PriceRange = product.priceRange;
-            let productPage = "/iphone/" + id;
-
-            let order1 = 1;
-            let order2 = 2;
-            if (order !== 1) {
-              order1 = 2;
-              order2 = 1;
-              order--;
-            } else {
-              order++;
-            }
+          {products.map((product, index) => {
+            const { Id, img, Url, Title, Brief, StartPrice, PriceRange } =
+              product;
+            const productPage = `/iphone/${Id}`;
 
             return (
               <div
-                key={id}
+                key={Id}
                 className="row justify-content-center text-center product-holder h-100 top-100 bottom-100"
               >
-                <div className={`col-sm-12 col-md-6 my-auto order-${order1}`}>
-                  <div className="product-title">{title}</div>
+                <div
+                  className={`col-sm-12 col-md-6 my-auto order-${
+                    index % 2 === 0 ? 1 : 2
+                  }`}
+                >
+                  <div className="product-title">{Title}</div>
                   <div className="product-brief">{Brief}</div>
                   <div className="starting-price">
                     {`Starting at ${StartPrice}`}
@@ -64,21 +58,22 @@ function Iphone() {
                   </div>
                 </div>
 
-                <div className={`col-sm-12 col-md-6 order-${order2}`}>
-                  {/* <div className="prodict-image"> */}
-                  <img
-                    src={img}
-                    alt=""
-                    // style={{ width: "100px", height: "80px" }}
-                  />
+                <div
+                  className={`col-sm-12 col-md-6 order-${
+                    index % 2 === 0 ? 2 : 1
+                  }`}
+                >
+                  <div className="product-image">
+                    <img src={img} alt={Title} />
+                  </div>
                 </div>
               </div>
-              // </div>
             );
           })}
         </div>
       </section>
-    </>
+    </div>
   );
 }
+
 export default Iphone;
--- src/Pages/Productpage/Productpage.jsx
+++ src/Pages/Productpage/Productpage.jsx
@@ -1,71 +1,57 @@
-// import React, { Component } from "react";
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import productsData from "../Iphone/products.json";
 
-const Productpage = () => {
-  const [prodactData, setProduct] = useState([]);
-  // const [productID] = useParams({pid});
+const SingleAppleProduct = () => {
+  const [productData, setProductData] = useState(null);
   const { pid } = useParams();
 
-  console.log(pid);
-
+  // useEffect(() => {
+  //   fetch("http://localhost:1111/products")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const singleProduct = data.filter((x) => x.id == pid);
+  //       setProduct(singleProduct);
+  //     });
+  // }, []);
   useEffect(() => {
-    fetch("http://localhost:1122/iphone")
-      .then((res) => res.json())
-      .then((data) => {
-        const singleProduct = data.filter((x) => x.id == pid);
-        setProduct(singleProduct);
-      });
-  }, []);
+    const singleProduct = productsData.find((x) => x.Id === pid);
+    setProductData(singleProduct);
+  }, [pid]);
+
+  if (!productData) return <div>Loading...</div>;
+
+  const { Id, img, Title, Brief, StartPrice, PriceRange, Description } =
+    productData;
 
   return (
     <div>
-      <div>
-        <section className="internal-page-wrapper top-100">
-          <div className="container">
-            {prodactData.map((product) => {
-              let id = product.id;
-              let title = product.title;
-              let img = product.img;
-              let Brief = product.brief;
-              let StartPrice = product.startingPrice;
-              let PriceRange = product.priceRange;
-              let details = product.description;
-
-              let productDiv = (
-                <div key={id} className="bottom-100">
-                  <div className="row justify-content-center text-center bottom-50">
-                    <div className="col-12">
-                      <div className="title-wraper bold">{title}</div>
-                      <div className="brief-description">{Brief}</div>
-                    </div>
-                  </div>
-
-                  <div className="row justify-content-center text-center product-holder h-100">
-                    <div className={`col-sm-12 col-md-6 my-auto`}>
-                      <div className="starting-price">
-                        {`Starting at ${StartPrice}`}
-                      </div>
-                      <div className="monthly-price">{PriceRange}</div>
-                      <div className="product-details">{details}</div>
-                    </div>
-
-                    <div className={"col-sm-12 col-md-6"}>
-                      <div className="prodict-image">
-                        <img src={img} />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-              return productDiv;
-            })}
+      <section className="internal-page-wrapper top-100">
+        <div className="container">
+          <div className="row justify-content-center text-center bottom-50">
+            <div className="col-12">
+              <div className="title-wrapper bold">{Title}</div>
+              <div className="brief-description">{Brief}</div>
+            </div>
           </div>
-        </section>
-      </div>
+
+          <div className="row justify-content-center text-center product-holder h-100">
+            <div className={`col-sm-12 col-md-6 my-auto`}>
+              <div className="starting-price">{`Starting at ${StartPrice}`}</div>
+              <div className="monthly-price">{PriceRange}</div>
+              <div className="product-details">{Description}</div>
+            </div>
+
+            <div className={`col-sm-12 col-md-6`}>
+              <div className="product-image">
+                <img src={img} alt={Title} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
 
-export default Productpage;
+export default SingleAppleProduct;
